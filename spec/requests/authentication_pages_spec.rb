1b2b79a9bf92aac46dc767d82e080a68f88d9261
Pablo Pazos <pablopr@gmail.com>
2014-01-31 12:02:05 +0100

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,11 +7,18 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+  
+  describe "signin" do
+    before { visit signin_path }
+
     describe "with invalid information" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
+      it { should have_error_message('Invalid') }
       
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -41,29 +48,6 @@ describe "Authentication" do
   end
   
   describe "authorization" do
-    describe "as admin user" do
-      let(:admin) { FactoryGirl.create(:admin) }
-      
-      before { sign_in admin, no_capybara: true }
-      
-      it "it should not be able to submitting DELETE request to the Users#destroy action to delete himself" do
-        expect do
-          delete user_path(admin)
-        end.to change(User, :count).by(0)
-      end
-    end
-    
-    describe "as non-admin user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:non_admin) { FactoryGirl.create(:user) }
-
-      before { sign_in non_admin, no_capybara: true }
-
-      describe "submitting a DELETE request to the Users#destroy action" do
-        before { delete user_path(user) }
-        specify { expect(response).to redirect_to(root_url) }
-      end
-    end
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -81,25 +65,49 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
-          
-          / Can't understand why this test fails 
-          describe "when signing in again" do
-            before do
-              delete signout_path
-              visit signin_path
-              fill_in "Email",    with: user.email
-              fill_in "Password", with: user.password
-              click_button "Sign in"
-            end
-
-            it "should render the default (profile) page" do
-              expect(page).to have_title(user.name)
-            end
-          end
-          /
         end
       end
       
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+      end
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+            
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -113,6 +121,30 @@ describe "Authentication" do
         end
       end
     end
+    
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin, no_capybara: true }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+    
+    describe "as admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      
+      before { sign_in admin, no_capybara: true }
+      
+      it "it should not be able to submitting DELETE request to the Users#destroy action to delete himself" do
+        expect do
+          delete user_path(admin)
+        end.to change(User, :count).by(0)
+      end
+    end 
     
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
